05eec61f8507f919fe2a7241241c2cfe04cf8d3b
shaharsolutions <shaharsolutions@gmail.com>
2026-02-25 12:32:38 +0200

    סידור כיוון חיצים בחלון הוספת אירוע

--- js/admin.js
+++ js/admin.js
@@ -3498,8 +3498,8 @@ function openCustomEventModal() {
          altFormat: "d/m/Y",
          allowInput: false,
          disableMobile: true,
-         prevArrow: "<svg viewBox='0 0 17 17'><path d='M5.207 8.471l7.146 7.147-0.707 0.707-7.853-7.854 7.854-7.853 0.707 0.707-7.147 7.146z' /></svg>",
-         nextArrow: "<svg viewBox='0 0 17 17'><path d='M13.207 8.472l-7.854 7.854-0.707-0.707 7.146-7.146-7.146-7.148 0.707-0.707 7.854 7.854z' /></svg>",
+         prevArrow: "<svg viewBox='0 0 17 17'><path d='M13.207 8.472l-7.854 7.854-0.707-0.707 7.146-7.146-7.146-7.148 0.707-0.707 7.854 7.854z' /></svg>",
+         nextArrow: "<svg viewBox='0 0 17 17'><path d='M5.207 8.471l7.146 7.147-0.707 0.707-7.853-7.854 7.854-7.853 0.707 0.707-7.147 7.146z' /></svg>",
          onOpen: function(selectedDates, dateStr, instance) {
             instance.calendarContainer.classList.add("premium-datepicker");
             instance.calendarContainer.setAttribute('dir', 'rtl');
@@ -3514,8 +3514,8 @@ function openCustomEventModal() {
          altFormat: "d/m/Y",
          allowInput: false,
          disableMobile: true,
-         prevArrow: "<svg viewBox='0 0 17 17'><path d='M5.207 8.471l7.146 7.147-0.707 0.707-7.853-7.854 7.854-7.853 0.707 0.707-7.147 7.146z' /></svg>",
-         nextArrow: "<svg viewBox='0 0 17 17'><path d='M13.207 8.472l-7.854 7.854-0.707-0.707 7.146-7.146-7.146-7.148 0.707-0.707 7.854 7.854z' /></svg>",
+         prevArrow: "<svg viewBox='0 0 17 17'><path d='M13.207 8.472l-7.854 7.854-0.707-0.707 7.146-7.146-7.146-7.148 0.707-0.707 7.854 7.854z' /></svg>",
+         nextArrow: "<svg viewBox='0 0 17 17'><path d='M5.207 8.471l7.146 7.147-0.707 0.707-7.853-7.854 7.854-7.853 0.707 0.707-7.147 7.146z' /></svg>",
          onOpen: function(selectedDates, dateStr, instance) {
             instance.calendarContainer.classList.add("premium-datepicker");
             instance.calendarContainer.setAttribute('dir', 'rtl');
