b15e018c31701835dff96f9bc5bacce8413451ae
shaharsolutions <shaharsolutions@gmail.com>
2026-03-23 12:26:29 +0200

    files organization adresses

---
+++ management/crm/sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'pension-net-crm-v1';
+const ASSETS = [
+    'pensionet-crm.html',
+    'favicon.png',
+    'js/config.js',
+    'js/supabaseClient.js'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
